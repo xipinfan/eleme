b4ed897c4dd3c86132f56b523691a49f699e1483
xipinfan <1310721316@qq.com>
2021-03-28 16:11:49 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
     component: Business,
     children:[
       {
-        path:'reserve',
+        path:'reserve', 
         component: Reserve,
         name:'reserve'
       },
